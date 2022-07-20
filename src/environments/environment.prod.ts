ec834dbbe902e5afda41ab35d361a365ccd30ea2
Thiago <thiago2508ss@gmail.com>
2022-07-20 09:45:26 -0300

    marvel api URL and public key

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://gateway.marvel.com:443/v1/public/',
+  apiKey: 'a4cd209e9211729ef19db258f3cdd50a'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://gateway.marvel.com:443/v1/public/',
+  apiKey: 'a4cd209e9211729ef19db258f3cdd50a'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
