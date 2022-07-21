cf4b2639ba4a1a532ae70f9277814f9abda84e5e
Thiago <thiago2508ss@gmail.com>
2022-07-21 10:58:22 -0300

    login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CharactersComponent } from './components/characters/characters.component';
 import { ComicsComponent } from './components/comics/comics.component';
 import { EventsComponent } from './components/events/events.component';
+import { LoginComponent } from './components/partials/login/login.component';
 import { SeriesComponent } from './components/series/series.component';
 import { StoriesComponent } from './components/stories/stories.component';
 
@@ -12,9 +13,16 @@ const routes: Routes = [
   { path: 'events', component: EventsComponent },
   { path: 'series', component: SeriesComponent },
   { path: 'stories', component: StoriesComponent },
-  { path: '', redirectTo: 'characters', pathMatch: 'full' },
-  { path: '**', component: CharactersComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: LoginComponent }
 ];
+
+// Client ID
+// 79389377198-a94b4roadn2pdkba9hnictrpvgh5f149.apps.googleusercontent.com
+
+// Client Secret
+// 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
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
